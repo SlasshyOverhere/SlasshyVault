7b7f1bf539fba2ec60cd5ddb51d2c75aa2efbba4
SlasshyOverhere <5boned00@gmail.com>
2026-02-17 16:08:54 +0530

    release: v3.0.17

--- src/components/UpdateNotesModal.tsx
+++ src/components/UpdateNotesModal.tsx
@@ -1,7 +1,7 @@
 import { motion, AnimatePresence } from 'framer-motion'
 import { X, Sparkles, CheckCircle, Bug } from 'lucide-react'
 
-const CURRENT_VERSION = '0.1.10'
+const CURRENT_VERSION = '3.0.17'
 
 interface UpdateNotesModalProps {
   open: boolean
